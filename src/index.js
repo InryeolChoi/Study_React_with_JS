03c45b74a0cda58706c45133b7ee7b6bf21d0963
InryeolChoi <dlsfuf0316@gmail.com>
2024-07-03 18:27:20 +0900

    ch14 ~ ch15

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import ProfileCard from './ch13/ProfileCard'
+import Blocks from './ch15/Blocks'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProfileCard />
+    <Blocks />
   </React.StrictMode>
 );
 
